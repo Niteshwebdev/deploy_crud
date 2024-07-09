667d134d1fb055c65e823b9f8f76d02e4607444b
Niteshwebdev <worldbest178@gmail.com>
2024-07-09 11:57:58 -0700

    Update showproduct.js

--- frontend/src/pages/showproduct.js
+++ frontend/src/pages/showproduct.js
@@ -17,7 +17,7 @@ const Showproduct = () => {
 
   const fetchProducts = async () => {
     try {
-      const response = await fetch(`https://deploy-crud-y5fr.vercel.app//showproductapi`);
+      const response = await fetch(`https://deploy-crud-y5fr.vercel.app/showproductapi`);
       const data = await response.json();
       setProducts(data);
       setLoading(false);
